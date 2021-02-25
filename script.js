1fc7857ff3a21eb29e4b26565a256843486a25e3
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-02-25 22:13:44 +0000

    functions added to script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubractionQuestion() {
+    
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivideQuestion() {
+    
+}
